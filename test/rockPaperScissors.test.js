7a6aa3de07cca640ec74bac1571a18bfe06e5997
Oana <oana.demian@gmail.com>
2023-08-07 22:26:31 +0100

    checks that playerOne wins when playerOneChoice is rock and playerTwoChoice is scissors.

--- test/rockPaperScissors.test.js
+++ test/rockPaperScissors.test.js
@@ -18,17 +18,19 @@ describe('Rock Paper Scissors Game Tests', () => {
   })
 
   it(`checks that playerOne wins when playerOneChoice is rock and playerTwoChoice is scissors.`, () => {
-    //arrange
-    const playerOne = 'Barbie';
-    const playerTwo = 'Ken';
-    const playerOneChoice = 'Rock';
-    const playerTwoChoice = 'Scissors';
+      //arrange
+      const playerOne = 'Barbie';
+      const playerTwo = 'Ken';
+      const playerOneChoice = 'Rock';
+      const playerTwoChoice = 'Scissors';
 
-    //act
-    const rockPaperScissors = new RockPaperScissors(playerOne, playerTwo);
-    const result = rockPaperScissors.game(playerOneChoice, playerTwoChoice);
+      //act
+      const rockPaperScissors = new RockPaperScissors(playerOne, playerTwo);
+      const result = rockPaperScissors.game(playerOneChoice, playerTwoChoice);
 
-    //assert
-    expect(result).to.equal(`${playerOne} played ${playerOneChoice}. ${playerTwo} played ${playerTwoChoice}. ${playerOne} wins.`);
+      //assert
+      expect(result).to.equal(`${playerOne} played ${playerOneChoice}. ${playerTwo} played ${playerTwoChoice}. ${playerOne} wins.`);
   })
+
+
 })
