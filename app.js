dce5b85e53706582a9a053972e69254a6d6678bd
Oana <oana.demian@gmail.com>
2023-08-09 16:07:32 +0100

    creates the index route

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express'; 
+import { router as indexRouter } from './routes/index.js';
+
 const port = 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/', indexRouter);
 app.listen(port, () => {
   console.log(`Rock Paper Scissors app listening at http://localhost:${port}`)
 });
