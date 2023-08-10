727da84c58ab14d6e9c668c058cad200703113f9
Oana <oana.demian@gmail.com>
2023-08-10 23:03:40 +0100

    refactor test suite

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,11 @@ import  chai from './chai-local.js';
 
 describe(`app router`, () => { 
   let testServer;
+  const playerOne = "Barbie"
+  const playerTwo = "Ken"
+  const playerOneChoice = "Rock"
+  const playerTwoChoice = "Paper"
+
   beforeEach(() => {
     testServer = chai.request(app).keepOpen();
   })
@@ -27,24 +32,26 @@ describe(`app router`, () => {
   it(`checks get request to the /game route renders the game page when player information successfully sent`, async () => {
     await testServer.post(`/game/new`).type('form')
     .send({
-      playerOne: "Barbie",
-      playerTwo: "Ken"
+      playerOne,
+      playerTwo
     })
     const res = await testServer.get(`/game`);
     expect(res).to.have.status(200);
     expect(res.text).to.contain(`Fight!`);
+    expect(res.text).to.contain(playerOne);
+    expect(res.text).to.contain(playerTwo);
     expect(res).to.be.html;
   });
 
   it(`update lastGame value when /game posted it`, async () => {
     await testServer.post(`/game`).type('form')
     .send({
-      playerOneChoice: "Rock",
-      playerTwoChoice: "Paper"
+      playerOneChoice,
+      playerTwoChoice
     })
     const res = await testServer.get(`/game`);
     expect(res).to.have.status(200);
-    expect(res.text).to.contain(`Barbie played Rock. Ken played Paper. Ken wins.`);
+    expect(res.text).to.contain(`${playerOne} played ${playerOneChoice}. ${playerTwo} played ${playerTwoChoice}. ${playerTwo} wins.`);
     expect(res).to.be.html;
   });
 });
