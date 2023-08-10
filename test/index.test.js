f444b247e4ea485d0530ed78e000e4453a9879c7
Oana <oana.demian@gmail.com>
2023-08-10 14:12:34 +0100

    checks express app renders an index page first

---
+++ test/index.test.js
@@ -0,0 +1,15 @@
+import { expect }  from 'chai';
+import  chaiHttp from 'chai-http';
+import { app } from '../server.js';
+import  chai from 'chai';
+
+chai.use(chaiHttp);
+
+describe(`app router tests`, () => {
+      const testApp = chai.request(app);
+      it(`should render index page first`, async () => {
+        const res = await testApp.get(`/`)
+          expect(res).to.have.status(200);
+          expect(res).to.be.html;
+      });
+  });
