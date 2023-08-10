433e7eed00a027d6c89d17bf4de97c0d5a99069b
Oana <oana.demian@gmail.com>
2023-08-10 14:12:02 +0100

    checks that posting to new game returns a redirect

---
+++ test/game.test.js
@@ -0,0 +1,24 @@
+import { expect }  from 'chai';
+import  chaiHttp from 'chai-http';
+import { app } from '../server.js';
+import  chai from 'chai';
+
+chai.use(chaiHttp);
+chai.should();
+describe(`game router tests`, () => {
+
+  it(`checks that posting to new game returns a redirect`, done => {
+    chai
+      .request(app)
+      .post("/game/new")
+      .send({
+        playerOne: "Barbie",
+        playerTwo: "Ken"
+      })
+      .redirects(0)
+      .end((err, res, body) => {
+        res.should.have.status(302);
+        done();
+      });
+  });
+});
